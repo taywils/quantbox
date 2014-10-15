ce84fb12a31823195a7a9689fe064fc145071297
DemetriousWilson <DemetriousWilson@gmail.com>
2014-10-15 16:34:12 -0700

    Simplify puppet to just use one file until I read more documentation

---
+++ manifests/init.pp
@@ -0,0 +1,14 @@
+class setup {
+
+    exec { "apt-update":
+      command => "/usr/bin/sudo apt-get -y update"
+    }
+
+    package {
+      [ "vim", "git-core", "build-essential"]:
+        ensure => ["installed"],
+        require => Exec['apt-update']
+    }
+}
+
+include setup
