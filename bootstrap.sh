69b729ce9af5c439dd1cb16460348f3124e39ead
DemetriousWilson <DemetriousWilson@gmail.com>
2014-10-16 11:14:59 -0700

    Update minor mistake with GCC install for bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -42,4 +42,8 @@ echo "=> Installing GCC"
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt-get update
 sudo apt-get install gcc-4.9 g++-4.9 cpp-4.9 -y
-sudo ln -s /user/bin/gcc-4.9 gcc
+# Clean up old build-essentials versions of GCC and add sym link
+cd /usr/bin
+sudo rm gcc g++ cpp
+sudo ln -s /usr/bin/gcc-4.9 gcc
+cd ~
